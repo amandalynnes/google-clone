7b84975604ab15d40ca671796bde384b338e3b7d
Amanda Simmons <carpe737diem@gmail.com>
2021-06-07 22:17:40 -0400

    working on google clone home page and search bar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Home from './pages/Home.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
+    //BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/search">
+              <h1>Searchpage</h1>
+            </Route>
+            <Route path="/">
+                <Home />          
+              </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
