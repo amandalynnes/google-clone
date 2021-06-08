a49eaab8276f62bfc074483e82f09e79667e9a4f
Amanda Simmons <carpe737diem@gmail.com>
2021-06-07 23:24:30 -0400

    added more styling to search buttons, also started capturing input from keyboard and console logging when search button is pressed

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
 import SearchIcon from "@material-ui/icons/Search"
 import MicIcon from "@material-ui/icons/Mic"
+import { Button } from '@material-ui/core'
 
 function Search() {
+    const [input, setInput] = useState('');
+
+    const search = e => {
+        e.preventDefault();
+
+        console.log('You hit the search button!')
+    }
     return (
         <div className="search">
             <div className="search_input">
                 <SearchIcon className="search_inputIcon" />
-                <input />
+                <input value={input} onChange={e => setInput(e.target.
+                    value)} />
                 <MicIcon />
             </div>
             
+            <div className="search_buttons">
+                <Button onClick={search} variant="outlined">Google Search</Button>
+                <Button variant="outlined">I'm Feeling Lucky</Button>
+
+            </div>
         </div>
     )
 }
