d386fd142d751c072fa7686142c078530533b15e
Amanda Simmons <carpe737diem@gmail.com>
2021-06-09 19:02:11 -0400

    set up serviceWorker and setting up reducer

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import './Search.css';
-import SearchIcon from "@material-ui/icons/Search";
-import MicIcon from "@material-ui/icons/Mic";
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
+import { useStateValue } from '../StateProvider';
+import { actionTypes } from '../reducer';
 
 function Search( { hideButtons = false } ) {
+    const [{}, dispatch] = useStateValue();
+
     const [input, setInput] = useState('');
     const history = useHistory();
 
@@ -14,10 +18,13 @@ function Search( { hideButtons = false } ) {
 
         console.log('You hit the search button! >> ', input)
 
+        dispatch({
+            type: actionTypes.SET_SEARCH_TERM,
+        })
         // do something with input.. come back and fix
         history.push('/search')
+    };
 
-    }
     return (
         <form className="search">
             <div className="search_input">
@@ -39,7 +46,7 @@ function Search( { hideButtons = false } ) {
                 type="submit" onClick={search} variant="outlined">Google Search</Button>
                 <Button className="search_buttonsHidden"
                 variant="outlined">I'm Feeling Lucky</Button>
-            </div> 
+            </div>
             )}
         </form>
     )
