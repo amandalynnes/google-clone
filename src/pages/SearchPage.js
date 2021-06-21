745df2c46c6735ea9e8e983f028a26320c3fe3a6
Amanda Simmons <carpe737diem@gmail.com>
2021-06-21 18:18:51 -0400

    search bar showing up at top of page, buttons hidden

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./SearchPage.css";
 import { useStateValue } from "../StateProvider";
 import useGoogleSearch from "../useGoogleSearch";
 import Response from "../response";
+import { Link } from "react-router-dom";
+import Search from "../components/Search";
+
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -18,12 +20,16 @@ function SearchPage() {
     <div className="searchPage">
       <div className="searchPage_header">
         <Link to="/">
-        <img
-          className="searchPage_logo"
-          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-          alt=""
-        />
+          <img
+            className="searchPage_logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+            alt=""
+          />
         </Link>
+
+        <div className="searchPage_headerBody">
+          <Search hideButtons />
+        </div>
       </div>
 
       <div className="searchPage_results"></div>
