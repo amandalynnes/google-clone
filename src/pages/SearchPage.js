d4294c0a4d2eebe8c4bc9834b19e2919a573b60a
Amanda Simmons <carpe737diem@gmail.com>
2021-06-22 16:22:30 -0400

    added all icon options under the search bar on the search page. changed styling of icons and removed text decoration of anchor tags

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -6,6 +6,11 @@ import Response from "../response";
 import { Link } from "react-router-dom";
 import Search from "../components/Search";
 import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 
 function SearchPage() {
@@ -37,11 +42,33 @@ function SearchPage() {
                     <SearchIcon />
                     <Link to="/all">All</Link>
                     </div>
+                    <div className="searchPage_option">
+                    <DescriptionIcon />
+                    <Link to="/news">News</Link>
+                    </div>
+                    <div className="searchPage_option">
+                    <ImageIcon />
+                    <Link to="/images">Images</Link>
+                    </div>
+                    <div className="searchPage_option">
+                    <LocalOfferIcon />
+                    <Link to="/shopping">shopping</Link>
+                    </div>
+                    <div className="searchPage_option">
+                    <RoomIcon />
+                    <Link to="/maps">maps</Link>
+                    </div>
+                    <div className="searchPage_option">
+                    <MoreVertIcon />
+                    <Link to="/more">more</Link>
+                    </div>
               </div>
               <div className="searchPage_optionsRight">
                   <div className="searchPage_option">
-                    <SearchIcon />
-                    <Link to="/all">All</Link>
+                    <Link to="/settings">Settings</Link>
+                    </div>
+                    <div className="searchPage_option">
+                    <Link to="/tools">Tools</Link>
                     </div>
               </div>
           </div>
