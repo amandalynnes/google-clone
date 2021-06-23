1da1a67e9b3b510c0baf05b4cc1c7f73721e8e26
Amanda Simmons <carpe737diem@gmail.com>
2021-06-23 15:12:36 -0400

    rending images pulled from the data object to the corresponding search results

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -83,7 +83,17 @@ function SearchPage() {
 
         {data?.items.map(item => (
           <div className="searchPage_result">
-            <a href={item.link}>
+            <a className="searchPage_resultLink"
+            href={item.link}>
+              {item.pagemap?.cse_image?.length > 0 && 
+              item.pagemap?.cse_image[0]?.src && (
+                <img className="searchPage_resultImage" 
+                src={item.pagemap?.cse_image?.length > 0 && 
+                  item.pagemap?.cse_image[0]?.src
+                }
+                alt=''
+                />
+              )}
             {item.displayLink} 
             </a>
             <a className="searchPage_resultTitle" href={item.link}>
